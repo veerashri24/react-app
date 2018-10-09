cb0c9701fca6b1161bd65007342bd3112ac95d03
veerashri24 <veerashri.patil@in-it1278.globant.com>
2018-10-09 12:16:30 +0530

    modal changes

--- my-hmn-app/components/title/index.js
+++ my-hmn-app/components/title/index.js
@@ -1,38 +1,77 @@
 import React from 'react'
+import Modal from 'react-modal'
 
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  }
+}
 
-const Title = () => (
-  <div className="title-wrapper">
-    <div className="title">
-      <span>Membership</span>
-    </div>
-    <div className="edit">
+class Title extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      show: false
+    }
+
+    this.showModal = this.showModal.bind(this)
+    this.hideModal = this.hideModal.bind(this)
+  }
+  showModal() {
+    this.setState({ show: true })
+  }
+
+  hideModal() {
+    this.setState({ show: false })
+  }
+
+  render() {
+    return (
+      <div className="title-wrapper">
+        <div className="title">
+          <span>Membership</span>
+        </div>
+        <div className="edit" onClick={this.showModal}>
           <span>Edit</span>
-    </div>
+        </div>
 
+        <style jsx>{`
+          .title-wrapper {
+            padding: 20px;
+            display: flex;
+            justify-content: space-between;
+            border-bottom: 1px solid #efefef;
+            color: #797a7e;
+          }
+          .title {
+            margin: 0;
+            width: 100%;
+            line-height: 1.15;
+            font-size: 16px;
+          }
+          ,
+          .edit {
+            padding-right: 7px;
+            right: 0;
+            top: 0;
+          }
+        `}</style>
+        <Modal
+          isOpen={this.state.show}
+          handleClose={this.hideModal}
+          style={customStyles}
+        >
+          <p>Modal</p>
+          <p>Data</p>
+        </Modal>
+      </div>
+    )
+  }
+}
 
-    <style jsx>{`
-      .title-wrapper {
-        padding: 20px;
-        display: flex;
-        justify-content: space-between;
-        border-bottom : 1px solid #efefef;
-        color: #797A7E;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        line-height: 1.15;
-        font-size: 16px;
-      },
-      .edit {
-	      padding-right: 7px;
-	      right: 0;
-	      top: 0;
-      }
-    `}</style>
-
-  </div>
-);
-
-export default Title;
+export default Title
--- my-hmn-app/pages/index.js
+++ my-hmn-app/pages/index.js
@@ -1,39 +1,47 @@
 import React from 'react'
 import Panel from '../components/panel'
 
-const Home = () => (
-  <div>
-    <div className="main">
-      <div className="panel-container">
-         <div className="panel-wrapper" >
-              <Panel />
-         </div>
-    </div>
-    </div>
+class Home extends React.Component {
+  constructor(props) {
+    super(props)
+  }
 
-    <style jsx>{`
-      .main {
-        width: 1200px;
-        margin-top: 20px;
-        height: auto;
-        margin : 0 auto;
-        background-color: #efefef;
-      }
-      .panel-container {
-        position:relative;
-        background:url('../static/images/bg.png') no-repeat;
-        height: 920px;
-      }
-      .panel-wrapper {
-        bottom:20px;
-        position: absolute;
-        width:548px;
-        height: 195px;
-        left: 50px;
-        background-color: #ffffff
-      }
-    `}</style>
-  </div>
-)
+  render() {
+    return (
+      <div>
+        <div className="main">
+          <div className="panel-container">
+            <div className="panel-wrapper">
+              <Panel />
+            </div>
+          </div>
+        </div>
+
+        <style jsx>{`
+          .main {
+            width: 1200px;
+            margin-top: 20px;
+            height: auto;
+            margin: 0 auto;
+            background-color: #efefef;
+          }
+          .panel-container {
+            position: relative;
+            background: url('../static/images/bg.png') no-repeat;
+            height: 920px;
+          }
+          .panel-wrapper {
+            bottom: 20px;
+            position: absolute;
+            width: 548px;
+            height: 195px;
+            left: 50px;
+            background-color: #ffffff;
+          }
+        `}</style>
+      </div>
+    )
+  }
+}
 
 export default Home
